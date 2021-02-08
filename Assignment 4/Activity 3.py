f594c32b455d947b4c0b0e939014f4ea6537c340
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2021-02-08 13:53:57 -0600

    Update Activity 3.py

--- Assignment 4/Activity 3.py
+++ Assignment 4/Activity 3.py
@@ -1,14 +1,18 @@
+# This program is used to take an input of miles and translate it into yards, feet, and inches. It also displays it in kilometers, meters, and centimeters.
+
 print("how many miles?")
 miles = float(input())
+
 yards = miles * 1760
 feet = miles * 5280
 inches = miles * 63360
 kilometers = miles * 1.60934
 meters = miles * 1609.33999997549
 centimeters = miles * 160933.99999754899181
-print("your length in yards is " + str(yards) + " yards,")
-print("your length in feet is " + str(feet) + " feet,")
-print("your length in inches is " + str(inches) + " inches,")
-print("your length in kilometers is " + str(kilometers) + " kilometers,")
-print("your length in meters is " + str(meters) + " meters,")
-print("your length in centimeters is " + str(centimeters) + " centimeters,")
+
+print("your length in yards is " + str(yards) + " yards")
+print("your length in feet is " + str(feet) + " feet")
+print("your length in inches is " + str(inches) + " inches")
+print("your length in kilometers is " + str(kilometers) + " kilometers")
+print("your length in meters is " + str(meters) + " meters")
+print("your length in centimeters is " + str(centimeters) + " centimeters")
