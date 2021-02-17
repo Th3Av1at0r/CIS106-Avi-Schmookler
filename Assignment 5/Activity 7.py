68fa9ae304a03b2ed5060821efc4237ad32e862a
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2021-02-17 12:51:55 -0600

    Update Activity 7.py

--- Assignment 5/Activity 7.py
+++ Assignment 5/Activity 7.py
@@ -1,19 +1,27 @@
-def dogyear(age):
-    dogyears = age * 7
+def getdogname():
     
-    return dogyears
+    print("What's your dog's name?")
+    dogname = (input())
     
+    return dogname
+    
+def displayresult(dogname,dogage):
+
+    print(str(dogname) + " is " + str(dogage) + " years old in dog years")
+    
+def getdogage():
+    
+    print("How many years old is your dog?")
+
+    humandogage = float(input())
+    dogage = humandogage *7
+    
+    return dogage
+
 # Main
 # This program takes your dogs name and age
 # and gives you the age in dog years
 
-
-print("What's your dog's name?")
-name = (input())
-
-print("How many years old is your dog?")
-age = float(input())
-
-dogage = dogyear(age)
-
-print(str(name) + " is " + str(dogage) + " years old in dog years")
+dogname = getdogname()
+dogage = getdogage()
+displayresult(dogname, dogage)
