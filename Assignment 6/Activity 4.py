b236fb9627f37235e922c1ea52da284ac0e96991
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2021-02-22 16:16:44 -0600

    Update Activity 4.py

--- Assignment 6/Activity 4.py
+++ Assignment 6/Activity 4.py
@@ -6,7 +6,7 @@
 
 def display_result(parallellogram_area, triangle_area):
     
-    print ("Your area as a parallellogram is " \
+    print("Your area as a parallellogram is " \
     + str(parallellogram_area))
     
     print ("Your area as a triangle is " \
@@ -15,23 +15,26 @@ def display_result(parallellogram_area, triangle_area):
     
 def get_length():
     
-    print ("What's your length?")
+    print("What's your length?")
     length = float(input())
 
     return length
     
+    
 def get_width():
     
-    print ("What's your width?")
+    print("What's your width?")
     width = float(input())
 
     return width
+    
     
 def get_parallellogram_area(length, width):
     
     parallellogram_area = length * width
     
     return parallellogram_area
+    
     
 def get_triangle_area(parallellogram_area):
     
