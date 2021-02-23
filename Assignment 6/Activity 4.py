46418103cf9df96734bf08423eb0e0005692771e
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2021-02-23 14:47:45 -0600

    Update Activity 4.py

--- Assignment 6/Activity 4.py
+++ Assignment 6/Activity 4.py
@@ -6,10 +6,10 @@
 
 def display_result(parallellogram_area, triangle_area):
     
-    print("Your area as a parallellogram is " \
+    print("Your area as a parallellogram is " 
     + str(parallellogram_area))
     
-    print("Your area as a triangle is " \
+    print("Your area as a triangle is " 
     + str(triangle_area))
     
     
