798079e73968b3b74d6ecff90674c3de3dd9b2f2
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2021-02-24 10:49:16 -0600

    Update Activity 1.py

--- Assignment 6/Activity 1.py
+++ Assignment 6/Activity 1.py
@@ -4,25 +4,25 @@
 # and your hourly rate and gives you
 # your pay over a week, month, and year
 
-def get_hours_per_week():
+def get_hours():
     
     print("how many hours do you work per week?")
-    hours_per_week = float(input())
+    hours = float(input())
     
-    return hours_per_week
+    return hours
     
     
-def get_hourly_rate():
+def get_rate():
     
     print("what is your hourly rate?")
-    hourly_rate = float(input())
+    rate = float(input())
     
-    return hourly_rate
+    return rate
    
     
-def get_weekly_pay(hourly_rate, hours_per_week):
+def get_weekly_pay(rate, hours):
 
-    weekly_pay = hourly_rate * hours_per_week
+    weekly_pay = rate * hours
     
     return weekly_pay
   
@@ -54,9 +54,9 @@ def display_result(weekly_pay, monthly_pay, yearly_pay):
 # main
 def main():
     
-    hourly_rate = get_hourly_rate()
-    hours_per_week = get_hours_per_week()
-    weekly_pay = get_weekly_pay(hourly_rate, hours_per_week)
+    rate = get_rate()
+    hours = get_hours()
+    weekly_pay = get_weekly_pay(rate, hours)
     yearly_pay = get_yearly_pay(weekly_pay)
     monthly_pay = get_monthly_pay(yearly_pay)
     display_result(weekly_pay, monthly_pay, yearly_pay)
