ba04e2e70b4ec667d259b967de7170b6b20f6a4c
Dave Braunschweig <dbraunsc@harpercollege.edu>
2021-02-25 07:59:29 -0600

    Assignment 6 formatting

--- Assignment 6/Activity 1.py
+++ Assignment 6/Activity 1.py
@@ -5,61 +5,45 @@
 # your pay over a week, month, and year
 
 def get_hours():
-    
     print("how many hours do you work per week?")
     hours = float(input())
-    
     return hours
-    
-    
+
+
 def get_rate():
-    
     print("what is your hourly rate?")
     rate = float(input())
-    
     return rate
-   
-    
-def get_weekly_pay(rate, hours):
 
+
+def get_weekly_pay(rate, hours):
     weekly_pay = rate * hours
-    
     return weekly_pay
-  
-    
+
+
 def get_monthly_pay(yearly_pay):
-    
     monthly_pay = yearly_pay / 12
-    
     return monthly_pay
-  
-    
+
+
 def get_yearly_pay(weekly_pay):
-    
     yearly_pay = weekly_pay * 52
-    
     return yearly_pay
 
 
 def display_result(weekly_pay, monthly_pay, yearly_pay):
+    print("your pay in a week is $" + str(weekly_pay) +
+        " your pay in a month is $" + str(monthly_pay) +
+        " your pay in a year is $" + str(yearly_pay))
 
-    print("your pay in a week is $" +
-        str(weekly_pay) +
-            " your pay in a month is $" +
-                str(monthly_pay) +
-                    " your pay in a year is $" +
-                        str(yearly_pay))
 
-    
-# main
 def main():
-    
     hours = get_hours()
     rate = get_rate()
     weekly_pay = get_weekly_pay(rate, hours)
     yearly_pay = get_yearly_pay(weekly_pay)
     monthly_pay = get_monthly_pay(yearly_pay)
     display_result(weekly_pay, monthly_pay, yearly_pay)
- 
+
 
 main()
--- Assignment 6/Activity 4.py
+++ Assignment 6/Activity 4.py
@@ -5,46 +5,33 @@
 # and a triangle with the same dimensions
 
 def display_result(parallellogram_area, triangle_area):
-    
-    print("Your area as a parallellogram is " +
-        str(parallellogram_area) +
-            "Your area as a triangle is" +   
-                str(triangle_area))
-    
-    
+    print("Your area as a parallellogram is " + str(parallellogram_area) +
+        "Your area as a triangle is" + str(triangle_area))
+
+
 def get_base():
-    
     print("What's your base?")
     base = float(input())
-
     return base
-    
-    
+
+
 def get_height():
-    
     print("What's your height?")
     height = float(input())
-
     return height
-    
-    
+
+
 def get_parallellogram_area(base, height):
-    
     parallellogram_area = base * height
-    
     return parallellogram_area
-    
-    
+
+
 def get_triangle_area(parallellogram_area):
-    
     triangle_area = parallellogram_area / 2
-    
     return triangle_area
-    
-    
-# main
+
+
 def main():
-    
     base = get_base()
     height = get_height()
     parallellogram_area = get_parallellogram_area(base, height)
