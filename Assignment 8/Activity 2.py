5cf1a999bbcd2eb9e81ee0cf7ffcadc6e2e90b1c
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2021-03-08 14:18:19 -0600

    Create Activity 2.py

---
+++ Assignment 8/Activity 2.py
@@ -0,0 +1,42 @@
+# This program takes grades and averages
+# them out to calculate the class average
+# Jess Elis helped debug this program
+
+def get_number_of_grades():
+    print("How many grades do you want to input?")
+    number_of_grades = int(input())
+    
+    return number_of_grades
+    
+
+def get_total(number_of_grades):
+    counter = 1
+    total = 0
+    while counter <= number_of_grades:
+        score = get_score()
+        total += score
+        counter += 1
+    
+    return total 
+    
+def get_score():
+    print("please input a score")
+    score = float(input())
+    
+    return score
+    
+
+def get_average(total, number_of_grades):
+    average = total / number_of_grades
+    
+    return average
+    
+
+def display_result(average):
+    print("the class average is " + str(average))
+    
+# main
+number_of_grades = get_number_of_grades()
+total = get_total(number_of_grades)
+average = get_average(total, number_of_grades)
+display_result(average)
