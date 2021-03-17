1267477ad7e1ea23b989a55682358017e70bc741
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2021-03-17 11:47:20 -0500

    Add files via upload

---
+++ Assignment 9/Activity #9-2.py
@@ -0,0 +1,53 @@
+def getcorrect():
+    print("Is it (h)igher, (l)ower, or (e)qual to " + str(guess) + " ?")
+    correct = input()
+    
+    return correct
+
+def getnewnumber(correct, highest, lowest, guess):
+    if correct == "h":
+        newnumber = guess + float(highest - lowest) / 2
+    else:
+        newnumber = guess - float(highest - lowest) / 2
+    
+    return newnumber
+
+def guesscalculation():
+    guess = 50
+    highest = 100
+    lowest = 0
+    correct = "ne"
+    while True:    #This simulates a Do Loop
+        correct = getcorrect
+        if correct == "h":
+            lowest = guess
+            newnumber = getnewnumber(correct, highest, lowest, guess)
+            guess = newnumber
+        else:
+            if correct == "l":
+                highest = guess
+                newnumber = getnewnumber(correct, highest, lowest, guess)
+                guess = newnumber
+            else:
+                if correct == "e":
+                    print("Yay that was the correct number!")
+                else:
+                    notvalid()
+        if not(correct != "e"): break   #Exit loop
+
+def notvalid():
+    print("That is not a valid input")
+
+def start():
+    ready = "Notready"
+    while ready != "ready":
+        print("Please think of a number between 1 and 100 and say ready when you are ready")
+        ready = input()
+        if ready == "ready":
+            guesscalculation()
+        else:
+            notvalid()
+
+# Main
+# This program guesses the random number that the user thinks of.
+start()
