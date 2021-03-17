401a7240d5d54a0f9552203e4b13e8b286b6fdb8
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2021-03-17 17:19:27 -0500

    Add files via upload

---
+++ Assignment 9/Activity #2.py
@@ -0,0 +1,40 @@
+def finish(correct):
+    if correct == "e":
+        print("Yay that was the correct number!")
+    else:
+        print("That is not a valid input")
+
+def getcorrect(guess):
+    print("Is it (h)igher, (l)ower, or (e)qual to " + str(guess) + " ?")
+    correct = input()
+    
+    return correct
+
+def getnewnumber(correct, highest, lowest, guess):
+    if correct == "h":
+        newnumber = guess + int(float(highest - lowest) / 2)
+    else:
+        newnumber = guess - int(float(highest - lowest) / 2)
+    
+    return newnumber
+
+# Main
+# This program guesses the random number that the user thinks of.
+guess = 50
+highest = 100
+lowest = 0
+correct = "ne"
+while True:    #This simulates a Do Loop
+    correct = getcorrect(guess)
+    if correct == "h":
+        lowest = guess
+        newnumber = getnewnumber(correct, highest, lowest, guess)
+        guess = newnumber
+    else:
+        if correct == "l":
+            highest = guess
+            newnumber = getnewnumber(correct, highest, lowest, guess)
+            guess = newnumber
+        else:
+            finish(correct)
+    if not(correct != "e"): break   #Exit loop
