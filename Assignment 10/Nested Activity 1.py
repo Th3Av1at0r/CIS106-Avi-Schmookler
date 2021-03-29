5a08976c3881e5de8fd853d9d65a8e31bc524710
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2021-03-29 11:43:33 -0500

    Update Nested Activity 1.py

--- Assignment 10/Nested Activity 1.py
+++ Assignment 10/Nested Activity 1.py
@@ -2,6 +2,7 @@
 # in the form of text, the size of the table
 # is up to the user's input
 
+
 def get_start():
     print("Where do you want the table to start?")
     start = input()
@@ -28,24 +29,24 @@ def get_if_line_1(start, end, length):
     while True:
         counter = get_line_1(start, end, counter)
         if not(counter == 1):
-            get_table(start, end, length,counter)
+            get_table(start, end, length, counter)
             break
 
 
 def get_line_1(start, end, counter):
-    for number in range(start,(end+1)):
+    for number in range(start, (end+1)):
         print("\t", number, end = "")
     counter = 2
         
     return counter
 
 
-def get_table(start, end, length,counter):
+def get_table(start, end, length, counter):
     if counter != length:
         print("\n")
-        for number_a in range(start,end+1):
+        for number_a in range(start, end+1):
             print(number_a)
-            for number_b in range(start,end+1):
+            for number_b in range(start, end+1):
                 print( "\t", number_a * number_b, end = "")
             print("\n")
     else:
@@ -54,7 +55,8 @@ def get_table(start, end, length,counter):
 def main():
     start = get_start()
     end = get_end()
-    length = get_length(start,end)
+    length = get_length(start, end)
     get_if_line_1(start, end, length)
+
 
 main()
