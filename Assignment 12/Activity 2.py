a415ca1f602c4c6e8d1b4f14147162450bff04d4
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2021-04-12 10:49:13 -0500

    Update and rename Activity 2 to Activity 2.py

---
+++ Assignment 12/Activity 2.py
@@ -0,0 +1,61 @@
+# when the user thinks of a number, this program guesses a new number higher 
+# or lower based on the user's input untill it is equal to the user's number
+
+def calculate_guess():
+    guess = 50
+    highest = 100
+    lowest = 0
+    correct = "ne"
+    count = 1
+    guesses = list()
+    guesses.append(50)
+    while True:
+        correct = getcorrect(guess)
+        if correct == "h":
+            lowest = guess
+            newnumber = getnewnumber(correct, highest, lowest, guess)
+            guess = newnumber
+            guesses.append(guess)
+        else:
+            if correct == "l":
+                highest = guess
+                newnumber = getnewnumber(correct, highest, lowest, guess)
+                guess = newnumber
+                guesses.append(guess)
+            else:
+                finish(correct, count, guesses)
+        count = count + 1
+        if not(correct != "e"): break
+
+    
+
+def finish(correct, count, guesses):
+    if correct == "e":
+        print("Yay that was the correct number!")
+        print("And it only took " + str(count) + " guesses!")
+        print("the guesses were " + str(guesses))
+    else:
+        print("That is not a valid input")
+        
+        
+def getcorrect(guess):
+    print("Is it (h)igher, (l)ower, or (e)qual to " + str(guess) + " ?")
+    correct = input()
+    
+    return correct
+    
+    
+def getnewnumber(correct, highest, lowest, guess):
+    if correct == "h":
+        newnumber = guess + int(float(highest - lowest) / 2)
+    else:
+        newnumber = guess - int(float(highest - lowest) / 2)
+    
+    return newnumber
+    
+    
+def main():
+    calculate_guess()
+
+
+main()
