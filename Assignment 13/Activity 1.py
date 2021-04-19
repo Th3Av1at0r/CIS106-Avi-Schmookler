926887ed6b708889df7d20d01dd75d6997d2844c
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2021-04-19 12:18:08 -0500

    Create Activity 1.py

---
+++ Assignment 13/Activity 1.py
@@ -0,0 +1,27 @@
+# this program takes the user's first and last name and transfers it into the 
+# format of Lastname, first initial.
+
+def get_full_name():
+    full_name = input("what is your first and last name?\n")
+    get_name(full_name)
+    
+    
+    
+def get_name(full_name):
+    name = full_name.split()
+    if (len(name) <= 1 or len(name) >= 3):
+        print("That is not a valid input, exiting code")
+        exit()
+    else:
+        display_result(name)
+
+
+def display_result(name):
+    first_initial = name[0]
+    print(name[1]+ ", " + first_initial[0] + ".")
+    
+    
+def main():
+    get_full_name()
+    
+main()
