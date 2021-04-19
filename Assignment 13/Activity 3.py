9efbf97be69b2bd1191c74817863d7ee7a8854a0
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2021-04-19 13:42:07 -0500

    Create Activity 3.py

---
+++ Assignment 13/Activity 3.py
@@ -0,0 +1,28 @@
+# this program asks for values seperated by commas
+# and prints them on seperate lines
+
+def get_values():
+    values = input("Please enter values seperated by commas.\n")
+    
+    return values
+    
+    
+def get_final_values(values):
+    seperate_values = values.replace(",", " ")
+    final_values = seperate_values.split()
+    
+    return final_values
+    
+    
+def display_result(final_values):
+    for elem in final_values:
+        print(elem)
+    
+    
+def main():
+    values = get_values()
+    final_values = get_final_values(values)
+    display_result(final_values)
+    
+
+main()
