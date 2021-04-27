8ca4261220720919364e2798f9ebb3202c804a7a
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2021-04-26 19:16:30 -0500

    Create Activity 1.py

---
+++ Assignment 14/Activity 1.py
@@ -0,0 +1,54 @@
+def get_scores_file():
+    scores_file = open("scores.txt", "r")
+    next(scores_file)
+    
+    return scores_file
+    
+
+def get_scores_lst(scores_file):
+    scores_lst = []
+    for line in scores_file:
+        lne_splt = line.split(",")
+        lne_1 = lne_splt[1]
+        lne_strp = lne_1.strip("\n")
+        lne_intr = int(lne_strp)
+        scores_lst.append(lne_intr)
+    
+    return scores_lst
+    
+
+def get_average_score(scores_lst):    
+    average_score = (sum(scores_lst) / len(scores_lst))
+    
+    return average_score
+    
+
+def get_highest_score(scores_lst):    
+    highest_score = max(scores_lst)
+    
+    return highest_score
+    
+    
+def get_lowest_score(scores_lst):
+    lowest_score = min(scores_lst)
+    
+    return lowest_score
+    
+    
+    
+def display_result(average_score, highest_score, lowest_score, scores_lst):
+    print("The scores are " + str(scores_lst) + "\nThe class average is " + 
+        str(average_score) + ", the highest grade in the class was " + 
+        str(highest_score) + ", and the lowest grade was " + str(lowest_score))
+    
+
+def main():
+    scores_file = get_scores_file()
+    scores_lst = get_scores_lst(scores_file)
+    average_score = get_average_score(scores_lst)
+    highest_score = get_highest_score(scores_lst)
+    lowest_score = get_lowest_score(scores_lst)
+    display_result(average_score, highest_score, lowest_score, scores_lst)
+    
+
+main()
