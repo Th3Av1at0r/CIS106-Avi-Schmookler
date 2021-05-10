aac2964c01d8e597d9554197fd96933934e9f8da
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2021-05-10 12:17:26 -0500

    Update final project.py

--- Final Project/final project.py
+++ Final Project/final project.py
@@ -1,70 +1,24 @@
 # This program takes information from a file containing a catalogue of CDs 
 # and prints the menu, number of items and average price
 
-def get_information_file(filename):
+def get_catalogue(filename):
     try:
         import os
-        information_file = open(filename, "r")
+        catalogue = open(filename, "r")
         filesize = os.path.getsize(filename)
         if filesize == 0:
             print("The file is empty, exiting code")
             exit()
-        else:
-            next(information_file)
-            next(information_file)
-            
-    except IOError:
-        print("Error, file not accessible")
-        exit()
     
-    return information_file
+    return catalogue
 
-def get_title_list(information_file):
+def get_title_list(catalogue):
     title_list = []
-    for line in information_file:
-        if line.find("<TITLE>") != -1:
-            lne_strp_1 = line.strip("<TITLE>")
-            lne_strp_2 = lne_strp_1.strip("\n")
-            title_list.append(lne_strp_2)
-        else:
-            continue
-        
-    return title_list
-
-def get_artist_list(information_file):
-    artist_list = []
-    for line in information_file:
-        if line.find("<ARTIST>") != -1:
-            lne_strp_1 = line.strip("<ARTIST>")
-            lne_strp_2 = lne_strp_1.strip("\n")
-            artist_list.append(lne_strp_2)
-        else:
-            continue
-    return artist_list
+    information_file = ET.fromstring(catalogue)
+    title_list.append(catalogue.find('TITLE').text)
     
-def get_country_list(information_file):
-    country_list = []
-    for line in information_file:
-        if line.find("<COUNTRY>") != -1:
-            lne_strp_1 = line.strip("<COUNTRY>")
-            lne_strp_2 = lne_strp_1.strip("\n")
-            country_list.append(lne_strp_2)
-        else:
-            continue
-        
-    return country_list
 
-def get_company_list(information_file):
-    company_list = []
-    for line in information_file:
-        if line.find("<COMPANY>") != -1:
-            lne_strp_1 = line.strip("<COMPANY>")
-            lne_strp_2 = lne_strp_1.strip("\n")
-            company_list.append(lne_strp_2)
-        else:
-            continue
-        
-    return company_list
+    return title_list
 
 
 
@@ -77,15 +31,14 @@ def get_company_list(information_file):
 
 def main():
     filename = "cd_catalog.xml"
+    import xml.etree.ElementTree as ET
     information_file = get_information_file(filename)
     title_list = get_title_list(information_file)
-    artist_list = get_artist_list(information_file)
-    country_list = get_country_list(information_file)
-    company_list = get_company_list(information_file)
     print(title_list)
     
     
     
     
     
-main()
+main()    
+    
