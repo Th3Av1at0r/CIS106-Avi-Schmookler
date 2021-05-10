38e589f9dece746dbfe8aec8e9dffcb6a1782960
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2021-05-10 16:29:47 -0500

    Update final project.py

--- Final Project/final project.py
+++ Final Project/final project.py
@@ -18,6 +18,8 @@ def get_catalog(file_name):
             return catalog
     except:
         print("There was an error reading the file.")
+        exit()
+
 
 def get_title_list(catalog):
     information_file = ET.fromstring(catalog)
@@ -25,17 +27,20 @@ def get_title_list(catalog):
     
     return title_list
 
+
 def get_artist_list(catalog):
     information_file = ET.fromstring(catalog)
     artist_list = [el.text for el in information_file.findall('CD/ARTIST')]
 
     return artist_list
 
+
 def get_country_list(catalog):
     information_file = ET.fromstring(catalog)
     country_list = [el.text for el in information_file.findall('CD/COUNTRY')]
     
     return country_list
+
     
 def get_company_list(catalog):
     information_file = ET.fromstring(catalog)
@@ -43,17 +48,20 @@ def get_company_list(catalog):
     
     return company_list
 
+
 def get_price_list(catalog):
     information_file = ET.fromstring(catalog)
     price_list = [el.text for el in information_file.findall('CD/PRICE')]
     
     return price_list
+
     
 def get_year_list(catalog):
     information_file = ET.fromstring(catalog)
     year_list = [element.text for element in information_file.findall('CD/YEAR')]
     
     return year_list
+
 
 def get_average_price(price_list):
     price_list = [float(number) for number in price_list]
@@ -62,6 +70,20 @@ def get_average_price(price_list):
     
     return average_price
 
+
+def display_results(title_list, artist_list, country_list, company_list, 
+year_list, price_list, average_price):
+    counter = 0
+    while (counter < len(title_list)):
+        print(counter + 1, ": " + title_list[counter] + " - " + 
+        artist_list[counter] + " - " +country_list[counter] + " - " + 
+        company_list[counter] + " - $" +
+        price_list[counter] + " - " + year_list[counter])
+        counter += 1
+    print("There were " + str(len(title_list)) + 
+    " CDs and the average price is $" + average_price)
+    
+    
 def main():
     file_name = "cd_catalog.xml"
     catalog = get_catalog(file_name)
@@ -72,13 +94,10 @@ def main():
     price_list = get_price_list(catalog)
     year_list = get_year_list(catalog)
     average_price = get_average_price(price_list)
-    
-    
-    
-    
-    
-    
+    display_results(title_list, artist_list, country_list, company_list, 
+    year_list, price_list, average_price)
     
     
 main()    
+        
         
